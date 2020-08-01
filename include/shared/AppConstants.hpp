fc497f881b99d477417598ba068777a3788c1e56
dknb0709 <ibot07092@gmail.com>
2020-08-02 02:11:23 +0900

    fix spelling mistake

--- include/shared/AppConstants.hpp
+++ include/shared/AppConstants.hpp
@@ -7,5 +7,5 @@
 static const int W = 480;
 static const int H = 320;
 static const int PIXEL_SIZE = 1;
-static const char* TITLE = "Mandelbrot Exploler";
+static const char* TITLE = "Mandelbrot Explorer";
 static const Color DEFAULT_COLOR = {0x41, 0x69, 0xe1, 0xff};
