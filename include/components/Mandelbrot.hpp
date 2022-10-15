1252fa21735ad2be6ccffb2cc8ce5eacc3ec7fdc
dknb0709 <dknb0709@github.com>
2022-10-16 05:31:09 +0900

    multi-thread化

--- include/components/Mandelbrot.hpp
+++ include/components/Mandelbrot.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include "../common/Vec2.hpp"
+#include "../common/thread.hpp"
 #include "../shared/Color.hpp"
 #include "./Component.hpp"
 
@@ -12,13 +13,14 @@ struct Mandelbrot : Component {
   Vec2<double> scale;
   Vec2<double> worldTopLeft, worldBottomRight;
   int maxIteration = 64;
+  ThreadPool pool;
 
   Mandelbrot()
-    : offset{-2.2, -1.0}
-    , scale{static_cast<double>(W / 3.0), static_cast<double>(H / 2.0)}
-    , worldTopLeft{screenToWorld({0, 0})}
-    , worldBottomRight{screenToWorld({W, H})}
-  {
+      : offset{-2.2, -1.0},
+        scale{static_cast<double>(W / 3.0), static_cast<double>(H / 2.0)},
+        worldTopLeft{screenToWorld({0, 0})},
+        worldBottomRight{screenToWorld({W, H})},
+        pool{8} {
     this->addEventListener(EventType::MouseMove, [this](Event* ev) {
       auto mouseMove = static_cast<MouseMoveEvent*>(ev);
       if (Mouse::isHeld) {
@@ -66,54 +68,39 @@ struct Mandelbrot : Component {
   }
 
   Vec2<double> screenToWorld(const Vec2<int>& screen) {
-    return {(double)screen.x / scale.x + offset.x, (double)screen.y / scale.y + offset.y};
+    return {(double)screen.x / scale.x + offset.x,
+            (double)screen.y / scale.y + offset.y};
   }
 
-  // void render(std::array<uint32_t, W * H>& pixels) override {
-  //   for (int y = 0; y < H; ++y) {
-  //     for (int x = 0; x < W; ++x) {
-  //       auto world = screenToWorld({x, y});
-  //       std::complex<double> z(0.0, 0.0);
-  //       std::complex<double> c(world.x, world.y);
-  //       int n = 0;
-  //       while (abs(z) < 2.0 && n < maxIteration) {
-  //         z = z * z + c;
-  //         n++;
-  //       }
-  //       if (n == maxIteration) {
-  //         // 収束したと判断 → 黒く塗りつぶす
-  //         pixels[x + W * y] = 0;
-  //       } else {
-  //         // 発散した場合は発散する速さをもとに着色
-  //         auto color = jetColor(n);
-  //         pixels[x + W * y] = color.get();
-  //       }
-  //     }
-  //   }
-  // }
-
   virtual void render(std::array<uint32_t, W * H>& pixels) override {
-    for (int y = 0; y < H; ++y) {
-      for (int x = 0; x < W; ++x) {
-        const auto world = screenToWorld({x, y});
-        auto zr = double{0.0}, zi = double{0.0};
-        auto cr = world.x, ci = world.y;
-        int iter = 0;
-        while (zr * zr + zi * zi < 4.0 && iter < maxIteration) {
-          const double zr2 = zr * zr, zi2 = zi * zi, zri = zr * zi;
-          zr = zr2 - zi2 + cr;
-          zi = zri + zri + ci;
-          iter++;
-        }
-        if (iter == maxIteration) {
-          // 収束したと判断 → 黒く塗りつぶす
-          pixels[x + W * y] = 0;
-        } else {
-          // 収束しなかった場合は発散する速さをもとに着色
-          const Color color = jetColor(iter);
-          pixels[x + W * y] = color.get();
+    auto renderRegion = [&](int x1, int x2) {
+      for (int y = 0; y < H; ++y) {
+        for (int x = x1; x < x2; ++x) {
+          const auto world = screenToWorld({x, y});
+          auto zr = double{0.0}, zi = double{0.0};
+          auto cr = world.x, ci = world.y;
+          int iter = 0;
+          while (zr * zr + zi * zi < 4.0 && iter < maxIteration) {
+            const double zr2 = zr * zr, zi2 = zi * zi, zri = zr * zi;
+            zr = zr2 - zi2 + cr;
+            zi = zri + zri + ci;
+            iter++;
+          }
+          if (iter == maxIteration) {
+            // 収束したと判断 → 黒く塗りつぶす
+            pixels[x + W * y] = 0;
+          } else {
+            // 収束しなかった場合は発散する速さをもとに着色
+            const Color color = jetColor(iter);
+            pixels[x + W * y] = color.get();
+          }
         }
       }
+    };
+
+    int w = W / 8;
+    for (int i = 0; i < 8; ++i) {
+      renderRegion(w * i, w * i + w);
     }
   }
 };
